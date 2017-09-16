2cc27a5559ea0cc312460f023a4270143e50f45e
Mitchell Powell <mpowell48@users.noreply.github.com>
2017-09-16 00:27:04 -0400

    Update start.sh

--- source_code/start.sh
+++ source_code/start.sh
@@ -5,13 +5,16 @@
 
 # Purpose: Prep for and launch all necessary processes.
 
+# Specify where to send the DHT data.
 CSV=/media/pi/FEEDER_DATA/DHT_data.csv
 
+# If CSV is not a file, then create the file.
 if [ ! -f "$CSV" ] ; then
 	echo "Temperature,Humidity,TimeStamp" >> "$CSV"
 fi
 
-# Remove '#' and the space before it to redirect stderr & stdout to logs
+# Start running periodic.py, listener.py, and programed_feeding.py in the background.
+# Remove '#' and the space before it to redirect stderr & stdout to logs.
 ./periodic.py & #>./Logs/periodic.log
 ./listener.py & #>./Logs/listener.log
 ./programed_feeding.py &
